41ccc387fca1be7142f1ee312613fd1529950abe
pcrawfor <pcrawfor@gmail.com>
2015-02-27 18:27:41 -0700

    Implementation for a very basic key/value store that writes the vals to the specified filepath using gob encoding

---
+++ simple_store.go
@@ -0,0 +1,88 @@
+package simplestore
+
+import (
+	"bufio"
+	"encoding/gob"
+	"os"
+)
+
+type entry struct {
+	Key   string
+	Value interface{}
+}
+
+// SimpleStore is a basic key value store which writes to a gob file on local disk on save
+type SimpleStore struct {
+	entries  []*entry
+	filePath string
+}
+
+// NewSimpleStore instantiates a new store with the given filepath either creating or updating the given file
+func NewSimpleStore(filePath string) *SimpleStore {
+	s := SimpleStore{entries: []*entry{}, filePath: filePath}
+	s.loadEntries()
+	return &s
+}
+
+// Get retrieves the value for a given key or returns nil if the key does not exist
+func (s *SimpleStore) Get(key []byte) interface{} {
+	for _, v := range s.entries {
+		if string(key) == v.Key {
+			return v.Value
+		}
+	}
+
+	return nil
+}
+
+// Set sets the value of the given key in the store - only accepts string values
+func (s *SimpleStore) Set(key []byte, value interface{}) {
+	gob.Register(value)
+	for _, v := range s.entries {
+		if string(key) == v.Key {
+			v.Value = value
+			return
+		}
+	}
+
+	s.entries = append(s.entries, &entry{Key: string(key), Value: value})
+}
+
+// Save writes the current state of the store to the file on disk
+func (s *SimpleStore) Save() error {
+	f := s.getFile()
+	w := bufio.NewWriter(f)
+	enc := gob.NewEncoder(w)
+
+	err := enc.Encode(&s.entries)
+	if err != nil {
+		return err
+	}
+	w.Flush()
+	f.Close()
+	return nil
+}
+
+func (s *SimpleStore) loadEntries() error {
+	r := bufio.NewReader(s.getFile())
+	dec := gob.NewDecoder(r)
+	err := dec.Decode(&s.entries)
+	if err != nil {
+		if err.Error() == "EOF" {
+			s.entries = []*entry{}
+			return nil
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (s *SimpleStore) getFile() *os.File {
+	f, err := os.OpenFile(s.filePath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return f
+}
---
+++ simple_store_test.go
@@ -0,0 +1,98 @@
+package simplestore
+
+import (
+	"os"
+	"testing"
+)
+
+func initStore() *SimpleStore {
+	return NewSimpleStore("./test_store.gob")
+}
+
+func cleanupStore(t *testing.T) {
+	rerr := os.Remove("./test_store.gob")
+	if rerr != nil {
+		t.Error("Failed to cleanup test store")
+	}
+}
+
+func TestSimpleStoreSaveString(t *testing.T) {
+	store := initStore()
+
+	e := store.Save()
+	if e != nil {
+		t.Error("Error saving store " + e.Error())
+	}
+
+	store.Set([]byte("foo"), "bar")
+	v := store.Get([]byte("foo"))
+	if v != "bar" {
+		t.Error("Value not found")
+	}
+
+	v = store.Get([]byte("bar"))
+	if v != nil {
+		t.Error("Value should be nil")
+	}
+
+	e = store.Save()
+	if e != nil {
+		t.Error("Error saving store after write " + e.Error())
+	}
+
+	cleanupStore(t)
+}
+
+func TestSimpleStoreSaveOthers(t *testing.T) {
+	store := initStore()
+
+	store.Set([]byte("big"), map[string]int{"hi": 1})
+	v := store.Get([]byte("big"))
+	if v == nil {
+		t.Error("Value should not be nil")
+	}
+
+	m := v.(map[string]int)
+	if m["hi"] != 1 {
+		t.Error("stored map value is incorrect")
+	}
+
+	store.Set([]byte("small"), []bool{true, false, true})
+
+	v = store.Get([]byte("small"))
+	if v == nil {
+		t.Error("Value should not be nil")
+	}
+
+	a := v.([]bool)
+	if a[1] != false {
+		t.Error("Array value incorrect")
+	}
+
+	cleanupStore(t)
+}
+
+func TestSimpleStoreSaveAndLoad(t *testing.T) {
+	store := initStore()
+
+	store.Set([]byte("a"), map[string]string{"one": "two", "three": "four"})
+	e := store.Save()
+	if e != nil {
+		t.Error("Error saving ", e.Error())
+	}
+
+	store2 := initStore()
+	v := store2.Get([]byte("a"))
+
+	if v == nil {
+		t.Error("Value should not be nil")
+	}
+
+	m := v.(map[string]string)
+
+	if m["three"] != "four" {
+		t.Error("Error values loaded in store do not match")
+	}
+
+	cleanupStore(t)
+}
